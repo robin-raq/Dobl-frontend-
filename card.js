7a22416aa2ba2928b021f53af956280f519c113d
robin-raq <raqdrobinson@gmail.com>
2025-11-06 02:00:18 -0500

    continue with game code refactor and clean up

--- card.js
+++ card.js
@@ -25,6 +25,17 @@ const rewardDiv = createEl("div", "reward-div")
 
 deckArea.append(scoreCard, timer, resetButton, compCard, userCard)
 
+// =====================
+// Utility Functions
+// =====================
+
+function createEl(tag, className, text) {
+  const el = document.createElement(tag);
+  if (className) el.className = className;
+  if (text) el.innerText = text;
+  return el;
+}
+
 // === Game State ===
 let compArray = [];
 let myTimer = null;
@@ -34,8 +45,8 @@ let myTimer = null;
 // =====================
 
 function initGame() {
-  compArray = createCard(compCard, "pos");
-  createCard(userCard, "u-pos");
+  createCompCard()
+  createUserCard()
   startTimer();
   userCard.addEventListener("click", handleCardClick);
 }
@@ -86,18 +97,24 @@ function getEachEmoji(array, posOne, posTwo, posThree, posFour, posFive, posSix,
     posSeven.innerText = array[6]
     posEight.innerText = array[7]
 }
-
 // =====================
-// Utility Functions
+// Click + Score Logic
 // =====================
+const handleCardClick= (evt) => {
+    let clickedIcon = evt.target.innerText
+    console.log(clickedIcon)
 
-function createEl(tag, className, text) {
-  const el = document.createElement(tag);
-  if (className) el.className = className;
-  if (text) el.innerText = text;
-  return el;
+    if (compArray.includes(clickedIcon)){
+        let scoreNum = parseInt(score.innerText)
+        scoreNum += 100
+        score.innerText = scoreNum
+        createCompCard()
+        createUserCard()
+    }
+    else {
+        alert("that is not a match")
+    }
 }
-
 
 // =====================
 // Timer Logic
@@ -119,328 +136,289 @@ function runTimer() {
   }
 }
 
+// =====================
+// Initialize Game
+// =====================
+
+document.addEventListener("DOMContentLoaded", initGame);
 
 
-let scoreFormDiv = document.createElement("div")
-    scoreFormDiv.className = "form-div"
-
-// let scoresUL = document.createElement("ul")
-let highScoresDiv = document.createElement("div")
-highScoresDiv.className = "high-scores-div"
-
-let scoreButtonDiv = document.createElement("div")
-scoreButtonDiv.className = "end-button-div"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-myTimer = setInterval(() => runTimer(timeLeft), 1000);
-
-
-function runTimer(element){
-    element.innerText --
-
-    if (element.innerText <= 0) {
-
-        stopGamePlay()
-
-        clearInterval(myTimer)
-    }
-
-}
+// =====================
+// End of Game
+// =====================
 
 function stopGamePlay(){
-    //alert("Time's up!")
-    //userCard.innerHTML = 'GAME OVER'
-
-    resetButton.remove()
-
-    titleHOne.innerText = "GAME OVER!"
-
-    scoreCard.className = "final-score-class"
-
     timer.remove()
-
     userCard.remove()
     compCard.remove()
-
-
-    //view scores button
-    // let viewScoreButton = document.createElement("button")
-    // viewScoreButton.className = "large-end-button"
-    // viewScoreButton.innerText = "View High Scores"
-    // scoreButtonDiv.append(viewScoreButton)
-
-    //new game button
-
-
-    let newGameButton = document.createElement("button")
-    newGameButton.className = "end_buttons"
-    newGameButton.innerText = "Play Again"
+    resetButton.remove()
+    titleHOne.innerText = "GAME OVER!"
+    const newGameButton= createEl("button", "end_buttons", "Play Again!")
     endButtonDiv.append(newGameButton)
+    deckArea.append(endButtonDiv)
 
-
-
-    // event listener to reset page
     newGameButton.addEventListener("click", () => {
         window.location.reload(true);
     })
-
-
-
-
-
-
-
-
-
-    // let saveScoreForm = document.createElement("form")
-    // saveScoreForm.className = "score_form"
-
-    // let inputArea = document.createElement("INPUT")
-    // inputArea.type = "text"
-    // inputArea.name = "player_name"
-    // inputArea.placeholder = "ENTER NAME"
-    // let submitButton = document.createElement("Input")
-    // submitButton.type = "submit"
-
-
-
-    // saveScoreForm.append(inputArea, submitButton)
-    // scoreFormDiv.append(saveScoreForm)
-    // deckArea.append(scoreFormDiv, endButtonDiv, scoreButtonDiv)
-    deckArea.append(endButtonDiv)
-
-    // saveScoreForm.addEventListener("submit", (evt) => {
-    //     evt.preventDefault()
-    //     playerName = evt.target.player_name.value
-    //     playerScore = parseInt(score.innerText)
-
-    //     if (playerScore <= 300 ){
-    //         playerRewardId = 1
-    //     }
-    //     else if (playerScore <= 600 && playerScore > 300){
-    //         playerRewardId = 2
-    //     }
-    //     else if (playerScore <= 900 && playerScore > 600){
-    //         playerRewardId = 3
-    //     }
-    //     else if (playerScore <= 1200 && playerScore > 900){
-    //         playerRewardId = 4
-    //     }
-    //     else if (playerScore <= 1500 && playerScore > 1200){
-    //         playerRewardId = 5
-    //     }
-    //     else{
-    //         playerRewardId = 6
-    //     }
-
-
-
-
-    //     fetch(`https://dobble-game.herokuapp.com/scores`, {
-    //       method:'POST',
-    //      headers: {
-    //          'Content-type': 'application/json',
-    //          'accept': 'application/json'
-    //      },
-    //      body: JSON.stringify({
-    //         name: playerName,
-    //         value: playerScore,
-    //         reward_id: playerRewardId
-    //       })
-    //     })
-    //     .then(resp => resp.json())
-    //     .then(json_resp =>{
-    //         //console.log(json_resp.reward)
-    //         titleHOne.remove()
-    //         scoreCard.remove()
-    //         scoreFormDiv.remove()
-
-    //         newGameButton.className = "large-end-button"
-
-
-
-
-    //         let rewardTextDiv = document.createElement("div")
-    //         rewardTextDiv.className = "reward-text-div"
-    //         rewardTextDiv.innerText = `Good job. Here's some ${json_resp.reward.name}.`
-
-
-    //         // rewardDiv.innerText = `Congratulations ${playerName}! You've earned ${json_resp.reward.name}. `
-    //         let rewardImg = document.createElement("img")
-    //         rewardImg.src = json_resp.reward.image
-
-    //         rewardDiv.append(rewardTextDiv, rewardImg)
-    //         deckArea.prepend(rewardDiv)
-    //         deckArea.append(scoreButtonDiv)
-
-
-    //     })
-
-    // })
-
 }
 
 
 
 
-//deckArea.prepend(compCard)
-
-createCompCard()
-createUserCard()
-
-
-function createCompCard1(){
-    compCard.innerHTML = " "
-    let compPosOne = document.createElement("div")
-    compPosOne.className = "pos-one"
+// =====================
+// OLD CODE WITH SCORBOARD STUFF
+// =====================
 
 
-    let compPosTwo = document.createElement("div")
-    compPosTwo.className = "pos-two"
+// //Scoreboard Logic
 
-    let compPosThree = document.createElement("div")
-    compPosThree.className = "pos-three"
+// let scoreFormDiv = document.createElement("div")
+//     scoreFormDiv.className = "form-div"
 
-    let compPosFour = document.createElement("div")
-    compPosFour.className = "pos-four"
+// // let scoresUL = document.createElement("ul")
+// let highScoresDiv = document.createElement("div")
+// highScoresDiv.className = "high-scores-div"
 
-    let compPosFive = document.createElement("div")
-    compPosFive.className = "pos-five"
-
-    let compPosSix = document.createElement("div")
-    compPosSix.className = "pos-six"
-
-    let compPosSeven = document.createElement("div")
-    compPosSeven.className = "pos-seven"
-
-    let compPosEight = document.createElement("div")
-    compPosEight.className = "pos-eight"
-
-    compArray = randomProperty(deck, compPosOne, compPosTwo, compPosThree, compPosFour, compPosFive, compPosSix, compPosSeven, compPosEight)
-    // console.log(compArray)
-    compCard.append(compPosOne, compPosTwo, compPosThree, compPosFour, compPosFive, compPosSix, compPosSeven, compPosEight)
-}
+// let scoreButtonDiv = document.createElement("div")
+// scoreButtonDiv.className = "end-button-div"
 
 
 
+// function stopGamePlay1(){
+//     timer.remove()
+//     userCard.remove()
+//     compCard.remove()
+//     resetButton.remove()
+//     titleHOne.innerText = "GAME OVER!"
+//     const newGameButton= createEl("button", "end_buttons", "Play Again!")
+//     endButtonDiv.append(newGameButton)
+//     deckArea.append(endButtonDiv)
+
+//     newGameButton.addEventListener("click", () => {
+//         window.location.reload(true);
+//     })
 
 
 
-function createUserCard1(){
-    userCard.innerHTML = " "
-    let userPosOne = document.createElement("div")
-    userPosOne.className = "u-pos-one"
+//     //view scores button
+//     // scoreCard.className = "final-score-class"
+//     // let viewScoreButton = document.createElement("button")
+//     // viewScoreButton.className = "large-end-button"
+//     // viewScoreButton.innerText = "View High Scores"
+//     // scoreButtonDiv.append(viewScoreButton)
 
-    let userPosTwo = document.createElement("div")
-    userPosTwo.className = "u-pos-two"
+//     // let saveScoreForm = document.createElement("form")
+//     // saveScoreForm.className = "score_form"
 
-    let userPosThree = document.createElement("div")
-    userPosThree.className = "u-pos-three"
+//     // let inputArea = document.createElement("INPUT")
+//     // inputArea.type = "text"
+//     // inputArea.name = "player_name"
+//     // inputArea.placeholder = "ENTER NAME"
+//     // let submitButton = document.createElement("Input")
+//     // submitButton.type = "submit"
 
-    let userPosFour = document.createElement("div")
-    userPosFour.className = "u-pos-four"
-
-    let userPosFive = document.createElement("div")
-    userPosFive.className = "u-pos-five"
-
-    let userPosSix = document.createElement("div")
-    userPosSix.className = "u-pos-six"
-
-    let userPosSeven = document.createElement("div")
-    userPosSeven.className = "u-pos-seven"
-
-    let userPosEight = document.createElement("div")
-    userPosEight.className = "u-pos-eight"
-
-    randomProperty(deck, userPosOne, userPosTwo, userPosThree, userPosFour, userPosFive, userPosSix, userPosSeven, userPosEight)
-
-    userCard.append(userPosOne, userPosTwo, userPosThree, userPosFour, userPosFive, userPosSix, userPosSeven, userPosEight)
-}
+//     // saveScoreForm.append(inputArea, submitButton)
+//     // scoreFormDiv.append(saveScoreForm)
+//     // deckArea.append(scoreFormDiv, endButtonDiv, scoreButtonDiv)
 
 
+//     // saveScoreForm.addEventListener("submit", (evt) => {
+//     //     evt.preventDefault()
+//     //     playerName = evt.target.player_name.value
+//     //     playerScore = parseInt(score.innerText)
 
-userCard.addEventListener("click", (evt) => {
-    //console.log(evt.target)
-    let clickedIcon = evt.target.innerText
-    console.log(clickedIcon)
-
-    if (compArray.includes(clickedIcon)){
-        let scoreNum = parseInt(score.innerText)
-        scoreNum += 100
-
-       //++score.innerText
-       score.innerText = scoreNum
-        console.log(scoreCard.innerText)
-        createCompCard()
-        createUserCard()
-    }
-    else {
-        alert("that is not a match")
-    }
-
-
-})
-
-scoreButtonDiv.addEventListener("click", (evt) => {
-    //console.log(evt.target)
-    rewardDiv.remove()
-
-
-    fetch("https://dobble-game.herokuapp.com/scores")
-    .then(r => r.json())
-    .then((allScores) => {
-
-      let sorted =  allScores.sort((a,b) => {
-            return(b.value - a.value)
-        })
-        //console.log(sorted)
-        sorted.forEach((scoreObj) => {
-            displayScores(scoreObj)
-        })
-    })
-
-})
-
-//function to display high scores
-
-function displayScores(scoreObj){
-
-    rewardDiv.remove()
-    scoreButtonDiv.remove()
-    console.log(scoreButtonDiv)
-    // highScoresDiv.remove()
-
-    let scoreDiv = document.createElement("div")
-        scoreDiv.className = "each-score-div"
-
-        let scoreNameSpan = document.createElement("span")
-        scoreNameSpan.innerText = `Name: ${scoreObj.name}`
-
-        let divBreak = document.createElement("br")
-
-        let scoreValueSpan = document.createElement("span")
-        scoreValueSpan.innerText = `Points: ${scoreObj.value}`
-
-        scoreDiv.append(scoreNameSpan, divBreak, scoreValueSpan)
-        // scoreLi.innerText = `${scoreObj.name} w/ ${scoreObj.value} points`
-        highScoresDiv.append(scoreDiv)
-        deckArea.prepend(highScoresDiv)
+//     //     if (playerScore <= 300 ){
+//     //         playerRewardId = 1
+//     //     }
+//     //     else if (playerScore <= 600 && playerScore > 300){
+//     //         playerRewardId = 2
+//     //     }
+//     //     else if (playerScore <= 900 && playerScore > 600){
+//     //         playerRewardId = 3
+//     //     }
+//     //     else if (playerScore <= 1200 && playerScore > 900){
+//     //         playerRewardId = 4
+//     //     }
+//     //     else if (playerScore <= 1500 && playerScore > 1200){
+//     //         playerRewardId = 5
+//     //     }
+//     //     else{
+//     //         playerRewardId = 6
+//     //     }
 
 
 
 
-}
+//     //     fetch(`https://dobble-game.herokuapp.com/scores`, {
+//     //       method:'POST',
+//     //      headers: {
+//     //          'Content-type': 'application/json',
+//     //          'accept': 'application/json'
+//     //      },
+//     //      body: JSON.stringify({
+//     //         name: playerName,
+//     //         value: playerScore,
+//     //         reward_id: playerRewardId
+//     //       })
+//     //     })
+//     //     .then(resp => resp.json())
+//     //     .then(json_resp =>{
+//     //         //console.log(json_resp.reward)
+//     //         titleHOne.remove()
+//     //         scoreCard.remove()
+//     //         scoreFormDiv.remove()
+
+//     //         newGameButton.className = "large-end-button"
+
+
+
+
+//     //         let rewardTextDiv = document.createElement("div")
+//     //         rewardTextDiv.className = "reward-text-div"
+//     //         rewardTextDiv.innerText = `Good job. Here's some ${json_resp.reward.name}.`
+
+
+//     //         // rewardDiv.innerText = `Congratulations ${playerName}! You've earned ${json_resp.reward.name}. `
+//     //         let rewardImg = document.createElement("img")
+//     //         rewardImg.src = json_resp.reward.image
+
+//     //         rewardDiv.append(rewardTextDiv, rewardImg)
+//     //         deckArea.prepend(rewardDiv)
+//     //         deckArea.append(scoreButtonDiv)
+
+
+//     //     })
+
+//     // })
+
+// }
+
+
+// function runTimer1(element){
+//     element.innerText --
+
+//     if (element.innerText <= 0) {
+
+//         stopGamePlay()
+
+//         clearInterval(myTimer)
+//     }
+
+// }
+
+
+// function createCompCard1(){
+//     compCard.innerHTML = " "
+//     let compPosOne = document.createElement("div")
+//     compPosOne.className = "pos-one"
+
+
+//     let compPosTwo = document.createElement("div")
+//     compPosTwo.className = "pos-two"
+
+//     let compPosThree = document.createElement("div")
+//     compPosThree.className = "pos-three"
+
+//     let compPosFour = document.createElement("div")
+//     compPosFour.className = "pos-four"
+
+//     let compPosFive = document.createElement("div")
+//     compPosFive.className = "pos-five"
+
+//     let compPosSix = document.createElement("div")
+//     compPosSix.className = "pos-six"
+
+//     let compPosSeven = document.createElement("div")
+//     compPosSeven.className = "pos-seven"
+
+//     let compPosEight = document.createElement("div")
+//     compPosEight.className = "pos-eight"
+
+//     compArray = randomProperty(deck, compPosOne, compPosTwo, compPosThree, compPosFour, compPosFive, compPosSix, compPosSeven, compPosEight)
+//     // console.log(compArray)
+//     compCard.append(compPosOne, compPosTwo, compPosThree, compPosFour, compPosFive, compPosSix, compPosSeven, compPosEight)
+// }
+
+// function createUserCard1(){
+//     userCard.innerHTML = " "
+//     let userPosOne = document.createElement("div")
+//     userPosOne.className = "u-pos-one"
+
+//     let userPosTwo = document.createElement("div")
+//     userPosTwo.className = "u-pos-two"
+
+//     let userPosThree = document.createElement("div")
+//     userPosThree.className = "u-pos-three"
+
+//     let userPosFour = document.createElement("div")
+//     userPosFour.className = "u-pos-four"
+
+//     let userPosFive = document.createElement("div")
+//     userPosFive.className = "u-pos-five"
+
+//     let userPosSix = document.createElement("div")
+//     userPosSix.className = "u-pos-six"
+
+//     let userPosSeven = document.createElement("div")
+//     userPosSeven.className = "u-pos-seven"
+
+//     let userPosEight = document.createElement("div")
+//     userPosEight.className = "u-pos-eight"
+
+//     randomProperty(deck, userPosOne, userPosTwo, userPosThree, userPosFour, userPosFive, userPosSix, userPosSeven, userPosEight)
+
+//     userCard.append(userPosOne, userPosTwo, userPosThree, userPosFour, userPosFive, userPosSix, userPosSeven, userPosEight)
+// }
+
+
+// scoreButtonDiv.addEventListener("click", (evt) => {
+//     //console.log(evt.target)
+//     rewardDiv.remove()
+
+
+//     fetch("https://dobble-game.herokuapp.com/scores")
+//     .then(r => r.json())
+//     .then((allScores) => {
+
+//       let sorted =  allScores.sort((a,b) => {
+//             return(b.value - a.value)
+//         })
+//         //console.log(sorted)
+//         sorted.forEach((scoreObj) => {
+//             displayScores(scoreObj)
+//         })
+//     })
+
+// })
+
+// //function to display high scores
+
+// function displayScores(scoreObj){
+
+//     rewardDiv.remove()
+//     scoreButtonDiv.remove()
+//     console.log(scoreButtonDiv)
+//     // highScoresDiv.remove()
+
+//     let scoreDiv = document.createElement("div")
+//         scoreDiv.className = "each-score-div"
+
+//         let scoreNameSpan = document.createElement("span")
+//         scoreNameSpan.innerText = `Name: ${scoreObj.name}`
+
+//         let divBreak = document.createElement("br")
+
+//         let scoreValueSpan = document.createElement("span")
+//         scoreValueSpan.innerText = `Points: ${scoreObj.value}`
+
+//         scoreDiv.append(scoreNameSpan, divBreak, scoreValueSpan)
+//         // scoreLi.innerText = `${scoreObj.name} w/ ${scoreObj.value} points`
+//         highScoresDiv.append(scoreDiv)
+//         deckArea.prepend(highScoresDiv)
+
+
+
+
+// }
 
 
